2ee2b5893548bbabcb320a87a781d390804994c9
FelixDolderer <felix.dolderer@sup-logistik.de>
2024-12-01 23:13:57 +0100

    websocket get individual task

--- app/pages/tasks/[taskId].vue
+++ app/pages/tasks/[taskId].vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { useWebSocket } from '@vueuse/core';
 import { useRouteParams } from '@vueuse/router';
+import { TOPICS } from '~~/schemas';
+import { tasksTopicSchema, type Task } from '~~/schemas/tasks';
 
 definePageMeta({
   validate: async (route) => {
@@ -8,11 +11,50 @@ definePageMeta({
 })
 
 const taskId = useRouteParams('taskId', '', { transform: Number })
+const task = ref<Task>()
+
+// #region WebSockets
+
+const { host } = useRequestURL()
+const { data, close } = useWebSocket(`ws://${host}/api/ws/tasks/${taskId.value}`)
+
+watch(data, () => {
+  console.log(data)
+  if (!(data.value instanceof Blob)) return
+
+  const reader = new FileReader()
+  reader.onload = () => {
+    const msgOptional = tasksTopicSchema.safeParse(
+      JSON.parse(reader.result?.toString() || ""),
+    )
+    if (!msgOptional.success) return
+    const msg = msgOptional.data
+
+    if (msg.topic === TOPICS.TASKS.ID.GET) {
+      task.value = msg.data
+    } else if (msg.topic === TOPICS.TASKS.ID.UPDATE) {
+      task.value = msg.data
+    } else if (msg.topic === TOPICS.TASKS.ID.DELETE) {
+      // TODO: Do something useful.
+    }
+  }
+  reader.readAsText(data.value)
+})
+
+// #endregion WebSockets
+
+// #region Lifecycle
+
+onBeforeRouteLeave(() => close())
+onBeforeUnmount(close)
+
+// #endregion Lifecycle
 </script>
 
 <template>
   <div>
-    {{ taskId }}
+    <ClientOnly v-if="task!!">
+      <TaskDetails :task="task" />
+    </ClientOnly>
   </div>
 </template>
-
---
+++ server/api/ws/tasks/[taskId].ts
@@ -0,0 +1,29 @@
+import { z } from "zod"
+import { COMMANDS, TOPICS } from "~~/schemas"
+import { tasksCommandSchema } from "~~/schemas/tasks"
+import { addTask, deleteTask, getTask } from "~~/server/ws/tasks"
+
+const API_WS_TASKS_ID_URL = "/api/ws/tasks/"
+
+export default defineWebSocketHandler({
+    open(peer) {
+        const unparsedTaskId = peer.websocket.url?.split(API_WS_TASKS_ID_URL)[1]
+        const taskId = z.coerce.number().parse(unparsedTaskId)
+        peer.subscribe(TOPICS.TASKS.ID.CHANNEL(taskId))
+        getTask(peer, { id: taskId })
+    },
+    message(peer, rawMessage) {
+        const message = tasksCommandSchema.parse(rawMessage.json())
+        const { command } = message
+        if (command === COMMANDS.TASKS.ID.UPDATE) {
+            addTask(peer, message.data)
+        } else if (command === COMMANDS.TASKS.ID.DELETE) {
+            deleteTask(peer, message.data)
+        }
+    },
+    close(peer) {
+        const unparsedTaskId = peer.websocket.url?.split(API_WS_TASKS_ID_URL)[1]
+        const taskId = z.coerce.number().parse(unparsedTaskId)
+        peer.unsubscribe(TOPICS.TASKS.ID.CHANNEL(taskId))
+    },
+})
